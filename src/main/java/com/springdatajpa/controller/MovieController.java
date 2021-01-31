de4027235034cd0b23805fa442ae0872eb7d84dc
Ruslan <gundertajlo260@gmail.com>
2021-01-31 05:45:56 +0200

    java-hw-4

--- src/main/java/com/springdatajpa/controller/MovieController.java
+++ src/main/java/com/springdatajpa/controller/MovieController.java
@@ -4,8 +4,6 @@ import com.springdatajpa.entyty.Movie;
 import com.springdatajpa.service.IMovieService;
 import com.springdatajpa.validator.MovieValidator;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.WebDataBinder;
